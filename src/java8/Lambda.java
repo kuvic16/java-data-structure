6792e04f4b7470c0aac3caf0c229783cfaeafec4
Shaiful Islam Palash <kuvic16@gmail.com>
2021-03-11 14:39:26 +0600

    java : labda expression

--- src/java8/Lambda.java
+++ src/java8/Lambda.java
@@ -1,11 +1,29 @@
 package java8;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @Author Shaiful Islam Palash | kuvic16@gmail.com
  * @CreatedAt: 3/11/2021
  */
 public class Lambda {
     public static void main(String[] args) {
-        
+        List<String> names = Arrays.asList("palash", "forhad", "jisan", "ridoy");
+        Collections.sort(names, new Comparator<String>(){
+            @Override
+            public int compare(String a, String b) {
+                return b.compareTo(a);
+            }
+        });
+
+        Collections.sort(names, (String a, String b) -> {
+            return b.compareTo(a);
+        });
+
+        Collections.sort(names, (String a, String b) -> b.compareTo(a));
+        Collections.sort(names, (a, b) -> b.compareTo(a));
     }
 }
