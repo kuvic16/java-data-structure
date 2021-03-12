a17282c5f78b83b50f90b48bedc59c602beefb23
Shaiful Islam Palash <kuvic16@gmail.com>
2021-03-12 12:05:48 +0600

    functional programming

--- src/java8/FunctionalIntrfc.java
+++ src/java8/FunctionalIntrfc.java
@@ -5,6 +5,20 @@ package java8;
  * @CreatedAt: 3/11/2021
  */
 
+/**
+ * What is functional programming?
+ * -----------------------------------
+ * It is a declarative style of programming rather than imperative. The basic objective of this style of programming is to
+ * make code more concise, less complex, more predictable, and easier to test compared to the legacy style of coding.
+ * Functional programming deals with certain key concepts such as *pure function*, *immutable state*, *assignment-less*
+ * programming.
+ *
+ *
+ *
+ * @param <F>
+ * @param <T>
+ */
+
 @FunctionalInterface
 interface Converter<F, T> {
     T convert(F form);
