df7a7c2d3435a25f4c967b3971a789f9b16a93c6
Shaiful Islam Palash <kuvic16@gmail.com>
2021-03-17 23:39:33 +0600

    Accessing fields and static variable from lambda

---
+++ src/java8/Lambda4.java
@@ -0,0 +1,16 @@
+package java8;
+
+/**
+ * @Author Shaiful Islam Palash | kuvic16@gmail.com
+ * @CreatedAt: 3/17/2021
+ */
+
+/**
+ * Accessing fields and static variables
+ *
+ * In contrast to local
+ */
+public class Lambda4 {
+    static int outerStaticNum;
+
+}
